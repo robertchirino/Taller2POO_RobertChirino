de8cef697f39746f2260fdd9956758775b5c4424
Robert <robert.chirino@alumnos.ucn.cl>
2026-04-13 14:40:53 -0400

    txts y tabla de tipos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
-
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int opcion;
+		do{
+			System.out.println("Bienvenido al simulador de monomones\r\n"+
+			"que desea hacer?");
+			System.out.println("1) Continuar");
+			System.out.println("2) Nueva Partida");
+			System.out.println("3) Salir");
+			System.out.print("opcion: ");
+			opcion = scanner.nextInt();
+			switch (opcion) {
+				case 1:
+					System.out.println("continuando partida...");
+					break;	
+				case 2:
+					System.out.println("iniciando nueva partida...");
+					break;
+				case 3:
+					System.out.println("saliendo...");
+					break;
+				default:
+					System.out.println("Opcion no valida");
+					break;
+			}
+
+		}while (opcion!=3);
 
 	}
 
